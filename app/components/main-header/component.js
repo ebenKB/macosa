6a8cfa147ddbe588705e88312c8b9e8c890123cf
ebenKB <eakbo23@gmail.com>
2019-06-28 15:45:48 +0000

    resolve linting errors

--- app/components/main-header/component.js
+++ app/components/main-header/component.js
@@ -1,4 +1,8 @@
 import Component from '@ember/component';
+import $ from 'jquery';
 
 export default Component.extend({
+  didInsertElement() {
+    $('.ui.dropdown').dropdown();
+  }
 });
--- app/components/vertical-menu/component.js
+++ app/components/vertical-menu/component.js
@@ -3,13 +3,39 @@ import { set } from '@ember/object';
 import $ from 'jquery';
 
 export default Component.extend({
-  didInsertElement() {
-    $('.ui.accordion').accordion({ "exclusive": true});
-  },
 
- options : null,
-  init() {
-    this._super(...arguments);
-    set(this, 'options', ['Human Resource', 'Marketing', 'Production',])
-  }
+	didInsertElement() {
+		$('.ui.accordion').accordion({ "exclusive": true});
+	},
+
+	options : null,
+	init() {
+		this._super(...arguments);
+		set(this, 'menus', 
+			[
+				{
+					name: 'Macosa',
+					submenus: [
+						{
+							name: 'Add item',
+							link: 'item',
+						},
+						{
+							name: 'Review item',
+							link: 'Review',
+						}
+					]
+				},
+				{
+					name: 'Marketing',
+					submenus: [
+						{
+							name: 'New Sales',
+							link: 'sales',
+						}
+					]
+				}
+			]
+		);
+	}
 });
