b2254cd881f622bf15d81a2897b17fc36cfbdcb3
ebenKB <eakbo23@gmail.com>
2019-07-11 17:17:57 +0000

    add url to confirm invitation

--- app/invitation/confirm/route.js
+++ app/invitation/confirm/route.js
@@ -2,7 +2,8 @@ import Route from '@ember/routing/route';
 import { get } from '@ember/object';
 
 export default Route.extend({
-  model() {
+  model({ token }) {
+    console.log('this is the token', token);
     // return get(this, 'store').findRecord('invitation', id);
     return get(this, 'store').createRecord('invitation', {
       name: 'Apotica',
