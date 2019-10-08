5117a3c1473c158b21f18d74b5c77d73c43a3e9e
ebenKB <eakbo23@gmail.com>
2019-10-08 18:01:07 +0000

    refactor entire UI

--- app/authenticated/index/route.js
+++ app/authenticated/index/route.js
@@ -1,5 +1,20 @@
 import Route from '@ember/routing/route';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
+import { inject as service } from '@ember/service';
 
 export default Route.extend(ApplicationRouteMixin, {
+  infinity: service(),
+  queryParams: {
+    user_id: {
+      refreshModel: true,
+    }
+  },
+  model(params) {
+    if (params.user_id !== null) {
+      return this.infinity.model('notification', {infinityCache: 60000, user_id: params.user_id});
+    } else {
+      return this.infinity.model('notification', {infinityCache: 60000});
+    }
+  },
 });
+
